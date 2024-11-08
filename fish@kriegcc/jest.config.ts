124f96cc72f22fd4399aebe4735e2b43b10047d5
kriegcc <github-kriegcc@krieg.cc>
2024-11-08 13:33:54 +0100

    Implement custom Jest test environment - WIP
    Incomplete. Does not work!

--- fish@kriegcc/jest.config.ts
+++ fish@kriegcc/jest.config.ts
@@ -1,27 +1,9 @@
-import type { Config } from "jest"
-// const UUID = __dirname.split("/").slice(-1)[0]
-
-const config: Config = {
+import { createDefaultPreset, JestConfigWithTsJest } from "ts-jest"
+const config: JestConfigWithTsJest = {
+  ...createDefaultPreset(),
   verbose: true,
-  preset: "ts-jest",
-  // fix in cjsEnvironment.js "const NodeEnvironment = require('jest-environment-node').TestEnvironment;""
-  testEnvironment: "cjs-jest-test-runtime",
-  clearMocks: true,
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
-  moduleDirectories: ["node_modules", "./src"],
-  // moduleNameMapper: {
-  //   "^lodash-es$": "lodash",
-  // },
-  // globals: {
-  //   "ts-jest": {
-  //     tsconfig: "./tsconfig.json",
-  //   },
-  //   __meta: {
-  //     uuid: UUID,
-  //   },
-  // },
+  testEnvironment: "./test/environments/cjsEnvironment.ts",
+  moduleDirectories: ["node_modules", "src", "test"],
 }
 
 export default config
